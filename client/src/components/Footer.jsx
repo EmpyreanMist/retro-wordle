c95a185e59bcfadd9d6c5275b85ff0888920ace1
Christian <christianfryksten1@gmail.com>
2025-04-14 19:42:30 +0200

    Fixes from Lighthouse

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,18 +1,29 @@
-import '../styles/footer.css';
+import "../styles/footer.css";
 
 function Footer() {
   return (
     <footer>
       <hr />
       <div className="icons">
-        <a href="https://github.com/EmpyreanMist" target="_blank">
-          <i className="fa-brands fa-github"></i>
+        <a
+          href="https://github.com/EmpyreanMist"
+          aria-label="Christian Fryksten on GitHub"
+          target="_blank"
+        >
+          <i className="fa-brands fa-github" aria-hidden="true"></i>
         </a>
-        <a href="https://linkedin.com/in/christian-fryksten" target="_blank">
-          <i className="fa-brands fa-linkedin"></i>
+        <a
+          href="https://linkedin.com/in/christian-fryksten"
+          aria-label="Christian Fryksten on LinkedIn"
+          target="_blank"
+        >
+          <i className="fa-brands fa-linkedin" aria-hidden="true"></i>
         </a>
-        <a href="mailto:christianfryksten1@gmail.com">
-          <i className="fa-solid fa-envelope"></i>
+        <a
+          href="mailto:christianfryksten1@gmail.com"
+          aria-label="Send email to Christian Fryksten"
+        >
+          <i className="fa-solid fa-envelope" aria-hidden="true"></i>
         </a>
       </div>
     </footer>
